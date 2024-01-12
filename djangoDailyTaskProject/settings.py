dad9f3d5aced62deaf55f1a14a0526416a976077
alixiq <154219840+alixiq@users.noreply.github.com>
2024-01-12 12:41:32 +0500

    Learning to create a new feature branch

--- djangoDailyTaskProject/settings.py
+++ djangoDailyTaskProject/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# Adding the swagger ui frame dependency !
 REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema' }
